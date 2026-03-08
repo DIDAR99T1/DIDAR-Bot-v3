1dc7f76ef2bbaff8718874245f2931cce55b8aa7
DIDAR99T1 <didar1didar234@gmail.com>
2026-03-08 15:59:27 +0600

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -63,7 +63,7 @@ module.exports = {
                                        `Type ${prefix}help <cmd> to see details.`;
 
                         msg += `\n\n⭔ Total Commands: ${totalCommands}\n⭔ ${helpHint}\n`;
-                        msg += `\n╭─✦ ADMIN: MahMUD 彡\n├‣ WHATSAPP\n╰‣ 01836298139`;
+                        msg += `\n╭─✦ ADMIN: Didar 彡\n├‣ WHATSAPP\n╰‣ 01337445694`;
 
                         try {
                                 const hh = await message.reply({ body: msg });
